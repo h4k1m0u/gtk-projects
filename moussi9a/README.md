8469c8a0abebe7ea2e548e02a4e334c78cd511c6
Hakim Benoudjit <h.benoudjit@gmail.com>
2020-06-06 12:18:27 +0200

    Select MP3 file from open dialog on button click

--- moussi9a/README.md
+++ moussi9a/README.md
@@ -6,8 +6,13 @@ GUI application made with PyGTK 3.0 to play/pause/stop an mp3 track loaded from 
 
 
 # Prerequisites
-- Python binding for VLC (LGPL)
+- Python binding for VLC (LGPL): [documentation][vlc-py-api] available.
 
 ```bash
 pip install python-vlc
 ```
+
+- Or read audio files with [GStreamer][gst].
+
+[vlc-py-api]: https://www.olivieraubert.net/vlc/python-ctypes/doc/
+[gst]: https://brettviren.github.io/pygst-tutorial-org/pygst-tutorial.html
--- moussi9a/moussi9a.py
+++ moussi9a/moussi9a.py
@@ -7,13 +7,32 @@ from gi.repository import Gtk, Gio
 
 class MyWindow(Gtk.Window):
     def __init__(self):
-        super().__init__(title='Moussi9a')
+        super().__init__()
         self.set_default_size(150, 100)
-        # self.set_border_width(10)
+
+        # header bar with title and close button
+        self.headerbar = Gtk.HeaderBar(title='Moussi9a')
+        self.headerbar.set_show_close_button(True)
+        self.set_titlebar(self.headerbar)
+
+        # opened song in vlc player & its filename
+        self.player = vlc.MediaPlayer()
+        self.filename = ''
+
+        # main layout
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=50)
+        self.add(box)
+
+        # toolbar with open button
+        toolbar = Gtk.Toolbar()
+        toolbar.set_style(Gtk.ToolbarStyle.ICONS)
+        button_open = Gtk.ToolButton(Gtk.STOCK_OPEN)
+        toolbar.insert(button_open, 0)
+        box.pack_start(toolbar, False, False, 0)
 
         # buttons container
-        grid_buttons = Gtk.Grid(row_spacing=10, column_spacing=10, margin=10)
-        self.add(grid_buttons)
+        grid_buttons = Gtk.Box()
+        box.pack_start(grid_buttons, False, False, 0)
 
         # icons & images
         icon_play = Gio.ThemedIcon(name='media-playback-start-symbolic')
@@ -24,27 +43,65 @@ class MyWindow(Gtk.Window):
             icon_pause, Gtk.IconSize.BUTTON)
 
         # play/pause buttons
-        button_play_pause = Gtk.ToggleButton()
-        button_play_pause.set_image(self.image_play)
-        grid_buttons.attach(button_play_pause, 0, 0, 1, 1)
-
-        # load mp3 song
-        self.song = vlc.MediaPlayer('music.mp3')
+        self.button_play_pause = Gtk.ToggleButton()
+        self.button_play_pause.set_image(self.image_play)
+        grid_buttons.pack_start(self.button_play_pause, False, False, 0)
 
         # attach signals to their handlers
         self.connect('destroy', self.on_destroy)
-        button_play_pause.connect('toggled', self.on_play_pause)
+        self.button_play_pause.connect('toggled', self.on_play_pause)
+        button_open.connect('clicked', self.on_open)
 
     def on_destroy(self, widget):
         Gtk.main_quit()
 
     def on_play_pause(self, widget):
-        if widget.get_active():
-            self.song.play()
-            widget.set_image(self.image_pause)
+        # no song is currently opened
+        if self.player.get_media() is None:
+            self.button_play_pause.set_active(False)
         else:
-            self.song.pause()
-            widget.set_image(self.image_play)
+            if widget.get_active():
+                self.player.play()
+                widget.set_image(self.image_pause)
+            else:
+                self.player.pause()
+                widget.set_image(self.image_play)
+
+    def on_open(self, widget):
+        # stop currently played song if opened
+        if self.player.get_media() is not None:
+            self.player.stop()
+            self.button_play_pause.set_image(self.image_play)
+            self.button_play_pause.set_active(False)
+
+        # file by extension
+        filter_mp3 = Gtk.FileFilter()
+        filter_mp3.set_name('MP3 files')
+        filter_mp3.add_mime_type('audio/mpeg')
+
+        # open file dialog
+        dialog = Gtk.FileChooserDialog(
+            'Choose an audio file',
+            self,
+            Gtk.FileChooserAction.OPEN,
+            (
+                Gtk.STOCK_CANCEL,
+                Gtk.ResponseType.CANCEL,
+                Gtk.STOCK_OK,
+                Gtk.ResponseType.OK
+            )
+        )
+        dialog.set_filter(filter_mp3)
+        response = dialog.run()
+
+        # get file name & destroy dialog
+        if response == Gtk.ResponseType.OK:
+            self.filename = dialog.get_filename()
+        dialog.destroy()
+
+        # load mp3 song
+        self.player.set_mrl(self.filename)
+        self.headerbar.set_subtitle(self.filename)
 
 
 if __name__ == '__main__':
